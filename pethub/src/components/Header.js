b141521169226abd12a3dd938c848d2c7fbea652
Kevin Pettibone <kevinpettibone5@gmail.com>
2023-01-25 12:53:43 -0600

    Added link to logo

--- pethub/src/components/Header.js
+++ pethub/src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import logo from "../photos/logo.jpg";
 
 function Header() {
   return (
     <header>
-      <img src={logo} alt="logo" className="logo" />
+      <Link to="/" target="_blank" rel="noreferrer">
+        <img src={logo} alt="logo" className="logo" />
+      </Link>
     </header>
   );
 }
