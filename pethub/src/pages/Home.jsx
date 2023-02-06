c58dfb326cf573eafe75ecf2a74e1749e040ee7b
Cliffp15 <Cliftonp15@yahoo.com>
2023-02-06 17:12:14 -0500

    Updated added Hero section and pet image selection

--- pethub/src/pages/Home.jsx
+++ pethub/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import HeroImage from "../photos/HeroImage.png"
+import caticon from "../photos/cat.png"
+import kitten from "../photos/kitten.jpg"
+import heart from "../photos/heart.png"
 
 
-const Home = () => {
+const Home = () => { 
+  
   return (
     <div>
       <div className="herosection">
@@ -14,10 +18,31 @@ const Home = () => {
           <input placeholder="Breed" type="text"/>
           <button className="searchbutton"> Search </button>
         </div>
-      </div> 
+      </div>
       <div className="featuredsection">
         <h1 className="featuredbanner">Featured Pets</h1>
-     
+          <div className="featuredpetimagesection">
+            {/* Convert into component */}
+            <div className="featuredpetimages">
+              <div className="petimage"> 
+                <img src={kitten} alt="kitten" />
+              </div>
+              <div className="peticon">
+                <img src={caticon} alt="caticon" />
+                </div>
+                {/* <div className="favoritebutton">
+                  <img src={heart} alt="caticon" />
+                </div> */}
+                <div className="petdetails">
+                  <h1 className="Name">Mini</h1>
+                  <h2 className="Breed&Age">Tabby, Young</h2>
+                  {/* <h2 className="Age"></h2>                 */}
+                  <h2 className="Gender">Female</h2>
+                  <h2 className="location">New York, New York</h2>
+                </div> 
+            </div>
+             {/* Convert into component */}
+          </div>
       </div>
     </div>
   );
