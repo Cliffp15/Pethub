da3864eff3f0f78a30dfc6264fc3d434b0c9a64e
edrickM <Edrickmiranda@gmail.com>
2023-02-08 12:10:47 -0600

    Server handles login

--- pethub/src/pages/LogIn.jsx
+++ pethub/src/pages/LogIn.jsx
@@ -1,25 +1,63 @@
 import React from "react";
-
+import { useState } from "react";
+import axios from "axios";
 
 const LogInPage = () => {
-  return (
-    <div>
-      <h1>Please Log in</h1>
-      <form>
-      <label>
-        <p>Username</p>
-        <input type="text" />
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" />
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-    </div>
-    
-  );
-  };
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    console.log("button was pressed");
+
+    e.preventDefault();
+
+    const data = {
+      userName: userName,
+      password: password,
+    };
+
+    axios({
+      method: "post",
+      url: "http://localhost:3001/login",
+      data: data,
+      headers: { "Content-Type": "application/json" },
+    })
+      .then(function (response) {
+        //handle success
+        console.log(response);
+        alert("User logged in successfully");
+      })
+      .catch(function (response) {
+        //handle error
+        console.log(response);
+        alert("An error occurred. Please try again.");
+      });
+};
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            placeholder="User Name"
+            type="text"
+            id="userName"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <input
+            placeholder="Password"
+            type="text"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit"> Sign Up</button>
+      </form>
+    );
+  
+};
 export default LogInPage;
