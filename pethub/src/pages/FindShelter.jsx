974b2a6587a06167a7f3ffc7fdcb60f6bd3a59e3
edrickM <Edrickmiranda@gmail.com>
2023-02-16 18:16:09 -0600

    function to generate shelter markers added

--- pethub/src/pages/FindShelter.jsx
+++ pethub/src/pages/FindShelter.jsx
@@ -37,15 +37,18 @@ const FindShelter = (props) => {
         'https://api.petfinder.com/v2/organizations',
         {
           headers: {
-            Authorization: `Authorization: Bearer 2EDep4nUnZldfKmEUG8j3YJlJcMG3dAlUZUA8IJeRA4BvbJ4z9`,
+            "Accept": "application/jason",
+            "Content-type": "application/jason",
+            Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIyRURlcDRuVW5abGRmS21FVUc4ajNZSmxKY01HM2RBbFVaVUE4SUplUkE0QnZiSjR6OSIsImp0aSI6Ijg3NzlmMjI4ZmI0YzAxOTExMWZlMzRlMjNhYWVjZTg0ODU3MGJhYzFhY2U3ODI5MzY3YzYyNTU2MWM2YWFlN2YzMzM1Nzk1NzQ4ZjgwOTViIiwiaWF0IjoxNjc2NTg4MjE5LCJuYmYiOjE2NzY1ODgyMTksImV4cCI6MTY3NjU5MTgxOSwic3ViIjoiIiwic2NvcGVzIjpbXX0.zjYSJA9FBEESs1a8V33LPP7yLY4cs2rL9LLm8IMFzTKIAocHlZ9ZmvycQsvEHWm4fD5Etyc4db064ob-xEkzYT8DAfg2mN9xL_F8k3DG564Ub196dNsFyfoT4A9yy0hMoru2gwfwqwPNA16dZAgm3i8NITN9WiK27pOhn3y_4iAD5tnD4U52BGjIOofpcLiES_0zT5X6Sz3rUWMfe-CWWx3PFETV0VyAWZhZzCsDzwPEVM_51WlqWfoH-BHAneNiQ3u-TH_eAjcsjvs3whsuD4DXdpFzs5JiIgDl_S7loz3fEWFMPYZVtCukdEEUZWVT7rIjtj93ChQokt8xKHZfhA"
           },
           params: {
             location: `${location.lat},${location.lng}`,
             distance: 25,
-            limit: 10,
+            limit: 15,
           },
         }
       );
+      // console.log(response.data)
       setShelters(response.data.organizations);
     };
     if (location) {
@@ -53,7 +56,36 @@ const FindShelter = (props) => {
     }
   }, [location]);
 
+  const getShelterCoordinates = async shelter => {
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodeURIComponent(
+        shelter.name
+      )}&inputtype=textquery&fields=geometry&key=AIzaSyBu3k86MKaUazcVMMNdeVmmdQO0pkSA2jE`
+    );
+    if (response.data.status === 'OK') {
+      const location = response.data.candidates[0].geometry.location;
+      console.log(response.data)
+      return { lat: location.lat, lng: location.lng };
+    } else {
+      return null;
+    }
+  };
+
+
   const Marker = () => <div style={{ color: 'red', fontSize: '32px' }}>📍</div>;
+
+  const renderMarkers = (map, maps) => {
+    shelters.forEach(async shelter => {
+      const coordinates = await getShelterCoordinates(shelter);
+      if (coordinates) {
+        new maps.Marker({
+          position: coordinates,
+          map,
+          title: shelter.name,
+        });
+      }
+    });
+  };
   
   return (
     <div style={{ height: '700px', width: '100%' }}>
@@ -64,6 +96,7 @@ const FindShelter = (props) => {
           bootstrapURLKeys={{ key:"AIzaSyBu3k86MKaUazcVMMNdeVmmdQO0pkSA2jE" }}
           defaultCenter={location}
           defaultZoom={16}
+          onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
         >
         <Marker lat={location.lat} lng={location.lng}></Marker>
         </GoogleMapReact>
