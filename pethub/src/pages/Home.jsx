16ff2655d0055a1b03ec8174cf534622949362a4
Kevin Pettibone <kevinpettibone5@gmail.com>
2023-03-02 17:46:20 -0600

    small ui changes

--- pethub/src/pages/Home.jsx
+++ pethub/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import HeroImage from "../photos/HeroImage.png";
 import dogIcon from "../photos/dog.png";
-import caticon from "../photos/cat.png";
 
 import "./styles/homepage.css";
 import { useState, useEffect } from "react";
@@ -27,7 +26,7 @@ const Home = () => {
           //Bearer token needs to be updated every hour for access to api
           // or it will produce 401 Error
           Authorization:
-            "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIyRURlcDRuVW5abGRmS21FVUc4ajNZSmxKY01HM2RBbFVaVUE4SUplUkE0QnZiSjR6OSIsImp0aSI6ImMyMTllNDhhYmFhMmUzOGViZGNlZTA1MWQzNWM5NzM5MmRiZDk1YmRiYmY5OGMxNDNlYjFiYzIxMTEyOWY5MTkzMDM5ZTk4MGFiM2JiOWYxIiwiaWF0IjoxNjc3Njk3NTEyLCJuYmYiOjE2Nzc2OTc1MTIsImV4cCI6MTY3NzcwMTExMiwic3ViIjoiIiwic2NvcGVzIjpbXX0.ZhLik-LATAYGUhyZaVuo3UCqax4n1C-TlZ6K1BQDpWx11ABLCVoSsZLj0eDwqp9O36j0XnnhZyDLUeR2Sr-5Aa0ZNviNufrFmfTddlSuYWqLWk6NrPYePt_QrQvqBUnuq5SN4p2YTSU5b_OI9E5UiMIf3_eRbp0vUGUJPU4ugp8FlZ6-XCtBhqV64JzsQR5oBMaFIe3-zpkV64YvKxeihVthjh0pypPizeWT4AtbWXtxsETJJCrJPJcCISBJN9kZifr8ZGz8v1lfWURiKd5YvmXG6f536XJPDLDrZoqKG1_EgEFKdruIN-dJKEamikKBkISx5WLhgvrfO8Nw5rUAaw",
+            "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJUUjhqVjg3NTl6aU82d1gxQ0pjUmRYWDN5WU9iWWNmZ1ZvUWt6UmhyMVlPbktmV0VtTSIsImp0aSI6ImEzYWM4Yzg0Y2Q2OGY0Yzg4MzJhNWQ1ZDZmNDZlOTkxN2ExNGVjZWI0ODdmZTNjMWQwOWU5ZDI3NTdmMWRjNzdhYTU2YzM2MjliNjZiMjk3IiwiaWF0IjoxNjc3Nzk2NzU1LCJuYmYiOjE2Nzc3OTY3NTUsImV4cCI6MTY3NzgwMDM1NSwic3ViIjoiIiwic2NvcGVzIjpbXX0.H7vd_T9xDWBsUYy2EkwOLz7VhvMvN8O5Fp-BkxFE2zpu1ZOdNHMycAHhip_4qcFzm6T41u_ZgAumaGugt_swqH2eU9bplZQBprhcAuRMKrKlU8AkBANOABIXgUgzAl3NHrd7v3WTCvWKRc4W9x1V6LiKbmiPZcIeLe9T2PFBU4vNuslKTHurx90MtA0MIzTDCKpmyPYYJXT1jblyJPQHijzS7PMUAGn44ocM9AufDEC8ByabufsUffDg9oG0MRqh99sSZKeqEKG_LhfbzBVFPrG9Ph07i-isfFOI4duRk6xA_g88glpX7TsGSbQbEC1MpjZebWn9MaqEVaAOBVydxg",
         },
       });
       console.log(`${API_URL}${animal}`);
--- pethub/src/pages/PetDetails.jsx
+++ pethub/src/pages/PetDetails.jsx
@@ -5,12 +5,12 @@ const ComponentDetails = () => {
   const { id } = useParams();
   const [component, setComponent] = useState(null);
 
-//   useEffect(() => {
-//     fetch(`https://api.petfinder.com/v2/animals/${id}`)
-//       .then(response => response.json())
-//       .then(data => setComponent(data))
-//       .catch(error => console.error(error));
-//   }, [id]);
+  //   useEffect(() => {
+  //     fetch(`https://api.petfinder.com/v2/animals/${id}`)
+  //       .then(response => response.json())
+  //       .then(data => setComponent(data))
+  //       .catch(error => console.error(error));
+  //   }, [id]);
 
   const Fetchpets = async () => {
     const response = await fetch(`https://api.petfinder.com/v2/animals/${id}`, {
@@ -22,11 +22,9 @@ const ComponentDetails = () => {
         //Bearer token needs to be updated every hour for access to api
         // or it will produce 401 Error
         Authorization:
-          "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIyRURlcDRuVW5abGRmS21FVUc4ajNZSmxKY01HM2RBbFVaVUE4SUplUkE0QnZiSjR6OSIsImp0aSI6IjBhYzgzMWIwZGMyYjc0MjVkOTljOWZjMjYxOTBiNTMxMmJlNDNmMjkyZGU0MmJhYzMwZjVjYTQ4YmMwNjgwNGM5MDM5NjczMzg0OGRiMGUxIiwiaWF0IjoxNjc3NjM2Nzc0LCJuYmYiOjE2Nzc2MzY3NzQsImV4cCI6MTY3NzY0MDM3NCwic3ViIjoiIiwic2NvcGVzIjpbXX0.Vgn_V1TgxDAgEbCBCTm4kRmSqFxHxlyy0Kl_5Xobahzqe7mBIdkZDfqQ9mk7AylsbdsLbe_lpCvLv970-lalSfsg2vn72st6ibRbqueaTrx_MAPzxgA_2KoZzUJBHbYTa4YkBpf-OcBzCUFByw-Fjmt9IsiFcExGhXLHxJIjuhdWGGp3etLjRhjRPZOGorKbJP_Q8IbLr4X8eXkl0Op3xovCkG-ZXHCvgr6druuoFtkkx_8RLKWzKGMj_VKEpb9O1AyLzEhnNBwcUiiMw0uLLm0gfY7asQpSfNhKKgkROAFep0iYZX1GiLP_hwy9UEBIxtQIgUyPlLHtvAnYe0VMsg"
+          "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJUUjhqVjg3NTl6aU82d1gxQ0pjUmRYWDN5WU9iWWNmZ1ZvUWt6UmhyMVlPbktmV0VtTSIsImp0aSI6ImEzYWM4Yzg0Y2Q2OGY0Yzg4MzJhNWQ1ZDZmNDZlOTkxN2ExNGVjZWI0ODdmZTNjMWQwOWU5ZDI3NTdmMWRjNzdhYTU2YzM2MjliNjZiMjk3IiwiaWF0IjoxNjc3Nzk2NzU1LCJuYmYiOjE2Nzc3OTY3NTUsImV4cCI6MTY3NzgwMDM1NSwic3ViIjoiIiwic2NvcGVzIjpbXX0.H7vd_T9xDWBsUYy2EkwOLz7VhvMvN8O5Fp-BkxFE2zpu1ZOdNHMycAHhip_4qcFzm6T41u_ZgAumaGugt_swqH2eU9bplZQBprhcAuRMKrKlU8AkBANOABIXgUgzAl3NHrd7v3WTCvWKRc4W9x1V6LiKbmiPZcIeLe9T2PFBU4vNuslKTHurx90MtA0MIzTDCKpmyPYYJXT1jblyJPQHijzS7PMUAGn44ocM9AufDEC8ByabufsUffDg9oG0MRqh99sSZKeqEKG_LhfbzBVFPrG9Ph07i-isfFOI4duRk6xA_g88glpX7TsGSbQbEC1MpjZebWn9MaqEVaAOBVydxg",
       },
     });
-
-
 
     const data = await response.json();
 
@@ -51,10 +49,12 @@ const ComponentDetails = () => {
 
   return (
     <div className="component-details">
-        <img src={component.photos[0]?.medium}></img>
+      <img src={component.photos[0]?.medium}></img>
       <h2>{component.name}</h2>
       <img src={component.imageUrl} alt={component.title} />
-      <h2 className="Breed&Age">{component.breeds.primary}, {component.age}</h2>
+      <h2 className="Breed&Age">
+        {component.breeds.primary}, {component.age}
+      </h2>
       <p>{component.description}</p>
     </div>
   );
