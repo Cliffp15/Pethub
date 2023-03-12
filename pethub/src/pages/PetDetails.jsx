0903444732edab5c96951a4947388ede73efef6e
Kevin Pettibone <kevinpettibone5@gmail.com>
2023-03-12 10:08:43 -0500

    added apopt button

--- pethub/src/pages/PetDetails.jsx
+++ pethub/src/pages/PetDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import "./styles/PetDetails.css";
 import { fetchToken } from "../api/petFinderToken";
 
 const ComponentDetails = () => {
@@ -61,6 +61,7 @@ const ComponentDetails = () => {
           {component.breeds.primary}, {component.age}
         </h2>
         <p className="pet-details-para">{component.description}</p>
+        <button className="adopt-button">Adopt {component.name}</button>
       </div>
     </div>
   );
