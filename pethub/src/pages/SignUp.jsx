1c6a03567f5bbdb1f5a063d1971c292d384089d9
Kevin Pettibone <kevinpettibone5@gmail.com>
2023-03-25 12:11:43 -0500

    improved sign up ui

--- pethub/src/pages/SignUp.jsx
+++ pethub/src/pages/SignUp.jsx
@@ -279,7 +279,10 @@
 
 // export default SignUp;
 
-import * as React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -289,10 +292,9 @@ import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import PetsIcon from "@mui/icons-material/Pets";
 import SignInButton from "../components/SignInButton";
 
 function Copyright(props) {
@@ -304,8 +306,8 @@ function Copyright(props) {
       {...props}
     >
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="/">
+        PetHub
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
@@ -316,13 +318,46 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignUp() {
-  const handleSubmit = (event) => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const { firstName, lastName, email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  let navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+
+    const newUser = {
+      firstName,
+      lastName,
+      email,
+      password,
+    };
+
+    try {
+      const response = await axios({
+        method: "post",
+        url: "http://localhost:3001/signup",
+        data: newUser,
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.status === 200) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.error(error);
+      console.log(error);
+      alert(error.response.data);
+    }
   };
 
   return (
@@ -337,8 +372,8 @@ export default function SignUp() {
             alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
+          <Avatar sx={{ m: 1, bgcolor: "#87ceeb" }}>
+            <PetsIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign up
@@ -359,6 +394,8 @@ export default function SignUp() {
                   id="firstName"
                   label="First Name"
                   autoFocus
+                  value={firstName}
+                  onChange={onChange}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -369,6 +406,8 @@ export default function SignUp() {
                   label="Last Name"
                   name="lastName"
                   autoComplete="family-name"
+                  value={lastName}
+                  onChange={onChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -379,6 +418,8 @@ export default function SignUp() {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
+                  value={email}
+                  onChange={onChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -390,6 +431,8 @@ export default function SignUp() {
                   type="password"
                   id="password"
                   autoComplete="new-password"
+                  value={password}
+                  onChange={onChange}
                 />
               </Grid>
             </Grid>
