ca6cd98beb71faca664d2c1e7a10175ccaf83c28
edrickM <Edrickmiranda@gmail.com>
2023-04-22 16:56:44 -0500

    updated styles

--- pethub/src/pages/FindPetPage.jsx
+++ pethub/src/pages/FindPetPage.jsx
@@ -29,6 +29,7 @@ const FindPetPage = () => {
   const [zip, setZip] = useState("");
   const [age, setAge] = useState("");
   const [gender, setGender] = useState("");
+  const [petsReturned, setPetsReturned] = useState(0);
 
   const Fetchpets = async (animal) => {
     setIsLoading(true);
@@ -46,6 +47,10 @@ const FindPetPage = () => {
       });
       const data = await response.json();
       console.log(data);
+
+      const petsReturned = data.pagination.total_count;
+      const formattedPetsReturned = petsReturned.toLocaleString();
+      setPetsReturned(formattedPetsReturned);
 
       const animals = Object.values(data.animals);
 
@@ -80,6 +85,9 @@ const FindPetPage = () => {
 
     const data = await response.json();
     console.log(data);
+    const petsReturned = data.pagination.total_count;
+    const formattedPetsReturned = petsReturned.toLocaleString();
+    setPetsReturned(formattedPetsReturned);
 
     if (Object.keys(data.animals).length === 0) {
       alert("No pets found!");
@@ -121,7 +129,7 @@ const FindPetPage = () => {
     const zipcodeInput = zip;
     const genderInput = gender;
     const ageInput = age;
-    
+
     console.log(animalInput, breedInput, zipcodeInput, genderInput, ageInput);
 
     let queryString = "";
@@ -146,7 +154,7 @@ const FindPetPage = () => {
       queryString += `&age=${ageInput}`;
     }
 
-    console.log("query",queryString);
+    console.log("query", queryString);
 
     if (queryString === "") {
       alert("Please Select A Filter");
@@ -191,186 +199,198 @@ const FindPetPage = () => {
 
   const handleSpeciesChange = (event) => {
     setSpecies(event.target.textContent);
-  }
+  };
 
   const handleBreedChange = (event) => {
-    
     if (event === null) {
       // Handle the case where no breed is selected
       console.log(event);
     } else {
       const selectedBreed = event.target.innerText;
       setBreed(selectedBreed);
-
     }
   };
 
   const handleAgechange = (event) => {
     setAge(event.target.innerText);
-  }
+  };
 
   const handleGenderChange = (event) => {
     setGender(event.target.innerText);
-  }
+  };
 
   const handleZipChange = (event) => {
     setZip(event.target.value);
-  }
-
+  };
 
   return (
     <div className="home-page">
       <div className="featured-section">
-        <h1 className="featured-banner" id="bannerid">
-          Find A Pet
-        </h1>
-        <div className="filterAndCards">
-          <div className="filtersContainer">
-            <label className="find-pet-select" for="animalinput">
-              Animal
-            </label>
-            <Select
-              onChange={handleSpeciesChange}
-              id="animalinput"
-              placeholder="Select"
-              indicator={<KeyboardArrowDown />}
-              sx={{
-                width: 240,
-                [`& .${selectClasses.indicator}`]: {
-                  transition: "0.2s",
-                  [`&.${selectClasses.expanded}`]: {
-                    transform: "rotate(-180deg)",
-                  },
-                },
-              }}
-            >
-              <Option value="dog">Dog</Option>
-              <Option value="cat">Cat</Option>
-              <Option value="bird">Bird</Option>
-              <Option value="rabbit">Rabbit</Option>
-            </Select>
-            <label className="find-pet-select" for="breedinput">
-              Breed
-            </label>
-            <Select
-              placeholder="Select"
-              id="breedinput"
-              onChange={handleBreedChange}
-              indicator={<KeyboardArrowDown />}
-              sx={{
-                width: 240,
-                [`& .${selectClasses.indicator}`]: {
-                  transition: "0.2s",
-                  [`&.${selectClasses.expanded}`]: {
-                    transform: "rotate(-180deg)",
-                  },
-                },
-              }}
-            >
-              {petBreeds.map((breed) => (
-                <Option key={breed} value={breed}>
-                  {breed.charAt(0).toUpperCase() + breed.slice(1)}
-                </Option>
-              ))}
-            </Select>
-
-            <label className="find-pet-select" for="genderinput">
-              Gender
-            </label>
-            <Select
-              id="genderinput"
-              placeholder="Select"
-              indicator={<KeyboardArrowDown />}
-              onChange={handleGenderChange}
-              sx={{
-                width: 240,
-                [`& .${selectClasses.indicator}`]: {
-                  transition: "0.2s",
-                  [`&.${selectClasses.expanded}`]: {
-                    transform: "rotate(-180deg)",
-                  },
-                },
-              }}
-            >
-              <Option value="">Select</Option>
-              <Option value="male">Male</Option>
-              <Option value="female">Female</Option>
-            </Select>
-
-            <label className="find-pet-select" for="ageinput">
-              Age
-            </label>
-            <Select
-              id="ageinput"
-              placeholder="Select"
-              indicator={<KeyboardArrowDown />}
-              onChange={handleAgechange}
-              sx={{
-                width: 240,
-                [`& .${selectClasses.indicator}`]: {
-                  transition: "0.2s",
-                  [`&.${selectClasses.expanded}`]: {
-                    transform: "rotate(-180deg)",
-                  },
-                },
-              }}
-            >
-              <Option value="">Select</Option>
-              <Option value="baby">Baby</Option>
-              <Option value="young">Young</Option>
-              <Option value="adult">Adult</Option>
-              <Option value="Senior">Senior</Option>
-            </Select>
-
-            <label className="find-pet-select" for="zipcodeinput">
-              Zipcode
-            </label>
-            <Input type="text" id="zip-input"
-            sx={{width: 240}} placeholder="Enter a zip code"
-            onChange={handleZipChange}
-            ></Input>
-            <Button className="filter-button" onClick={filterPets}
-            sx={{width: 240}}>
-              Filter Pets
-            </Button>
+        <h2 className="petsReturnedText">
+          We located {petsReturned} pets that are ready to be adopted!
+        </h2>
+        {isLoading ? (
+          <div className="loadingScreen">
+            <h1 className="loadingText">
+              Stay put while we fetch our furry friends
+            </h1>
+            <CircularProgress classname="loading" size="lg" />
           </div>
-          <div className="petCardDisplay">
-            {isLoading ? (
-              <div className="loading">
-                <CircularProgress size="lg" />
-              </div>
-            ) : petcard?.length > 0 ? (
-              <div className="findpetcardcontainer">
-                {petcard.map((petinfo, index) => (
-                  <PetCard key={index} petinfo={petinfo} />
+        ) : (
+          <div className="filterAndCards">
+            <div className="filtersContainer">
+              <label className="find-pet-select" for="animalinput">
+                Animal
+              </label>
+              <Select
+                onChange={handleSpeciesChange}
+                id="animalinput"
+                placeholder="Select"
+                indicator={<KeyboardArrowDown />}
+                sx={{
+                  width: 240,
+                  [`& .${selectClasses.indicator}`]: {
+                    transition: "0.2s",
+                    [`&.${selectClasses.expanded}`]: {
+                      transform: "rotate(-180deg)",
+                    },
+                  },
+                }}
+              >
+                <Option value="dog">Dog</Option>
+                <Option value="cat">Cat</Option>
+                <Option value="bird">Bird</Option>
+                <Option value="rabbit">Rabbit</Option>
+              </Select>
+              <label className="find-pet-select" for="breedinput">
+                Breed
+              </label>
+              <Select
+                placeholder="Select"
+                id="breedinput"
+                onChange={handleBreedChange}
+                indicator={<KeyboardArrowDown />}
+                sx={{
+                  width: 240,
+                  [`& .${selectClasses.indicator}`]: {
+                    transition: "0.2s",
+                    [`&.${selectClasses.expanded}`]: {
+                      transform: "rotate(-180deg)",
+                    },
+                  },
+                }}
+              >
+                {petBreeds.map((breed) => (
+                  <Option key={breed} value={breed}>
+                    {breed.charAt(0).toUpperCase() + breed.slice(1)}
+                  </Option>
                 ))}
-              </div>
-            ) : (
-              <div className="empty">No pets found.</div>
-            )}
-          </div>
-        </div>
-        <Button
-          className="pagination-button"
-          onClick={() => {
-            handlePreviousPage();
-            window.scrollTo(0, 950);
-          }}
-          disabled={currentPage === 1}
-        >
-          Previous
-        </Button>
+              </Select>
 
-        <Button
-          className="pagination-button"
-          onClick={() => {
-            handleNextPage();
-            window.scrollTo(0, 950);
-          }}
-        >
-          Next
-        </Button>
-        <h3> page: {currentPage}</h3>
+              <label className="find-pet-select" for="genderinput">
+                Gender
+              </label>
+              <Select
+                id="genderinput"
+                placeholder="Select"
+                indicator={<KeyboardArrowDown />}
+                onChange={handleGenderChange}
+                sx={{
+                  width: 240,
+                  [`& .${selectClasses.indicator}`]: {
+                    transition: "0.2s",
+                    [`&.${selectClasses.expanded}`]: {
+                      transform: "rotate(-180deg)",
+                    },
+                  },
+                }}
+              >
+                <Option value="">Select</Option>
+                <Option value="male">Male</Option>
+                <Option value="female">Female</Option>
+              </Select>
+
+              <label className="find-pet-select" for="ageinput">
+                Age
+              </label>
+              <Select
+                id="ageinput"
+                placeholder="Select"
+                indicator={<KeyboardArrowDown />}
+                onChange={handleAgechange}
+                sx={{
+                  width: 240,
+                  [`& .${selectClasses.indicator}`]: {
+                    transition: "0.2s",
+                    [`&.${selectClasses.expanded}`]: {
+                      transform: "rotate(-180deg)",
+                    },
+                  },
+                }}
+              >
+                <Option value="">Select</Option>
+                <Option value="baby">Baby</Option>
+                <Option value="young">Young</Option>
+                <Option value="adult">Adult</Option>
+                <Option value="Senior">Senior</Option>
+              </Select>
+
+              <label className="find-pet-select" for="zipcodeinput">
+                Zipcode
+              </label>
+              <Input
+                type="text"
+                id="zip-input"
+                sx={{ width: 240 }}
+                placeholder="Enter a zip code"
+                onChange={handleZipChange}
+              ></Input>
+              <Button
+                className="filter-button"
+                onClick={filterPets}
+                sx={{ width: 240 }}
+              >
+                Filter Pets
+              </Button>
+            </div>
+            <div className="petCardDisplay">
+              {petcard?.length > 0 ? (
+                <div className="findpetcardcontainer">
+                  {petcard.map((petinfo, index) => (
+                    <PetCard key={index} petinfo={petinfo} />
+                  ))}
+                </div>
+              ) : (
+                <div className="empty">No pets found.</div>
+              )}
+            </div>
+            
+          </div>
+        )}
+        <div className="pageButtonsContainer">
+          <Button
+            className="pagination-button"
+            onClick={() => {
+              handlePreviousPage();
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            }}
+            disabled={currentPage === 1}
+            sx={{ width: 75, margin: 2 }}
+          >
+            Previous
+          </Button>
+          <h3>{currentPage}</h3>
+          <Button
+            className="pagination-button"
+            onClick={() => {
+              handleNextPage();
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            }}
+            sx={{ width: 75, margin: 2 }}
+          >
+            Next
+          </Button>
+        </div>
       </div>
     </div>
   );
